9afbd1705c6f4f5bd51f6f97fcd8dec0e1fd616f
Motari2004 <hopefreymosingi1@gmail.com>
2026-01-31 15:57:25 +0300

    Describe what you changed here

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Install python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Install Playwright browser and system dependencies
 playwright install chromium
+playwright install-deps chromium
